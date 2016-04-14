b49e2e429cd92c0a54a3ab27939cfaa278f224bc
Alexandros Filios <alexanderfilios@hotmail.com>
2016-04-14 22:33:04 +0200

    Added http://www. to the beginnning of th URLs.

--- setup/extract_top_websites.sh
+++ setup/extract_top_websites.sh
@@ -5,13 +5,13 @@ alexa_1m="$setup_dir/resources/top-1m.csv"
 server_collection_url="http://127.0.0.1:3000/myapp_test1/first_parties2"
 
 # Find the indices (ranks) that we want to save (top 500 and 500 random)
-top_ranks=$(seq 1 4)
-random_ranks=$(shuf -i 501-1000000 -n 3 | sort -n)
+top_ranks=$(seq 1 500)
+random_ranks=$(shuf -i 501-1000000 -n 500 | sort -n)
 all_ranks=(${top_ranks[@]} ${random_ranks[@]})
 #all_ranks=(1)
 export IFS=","
 for r in ${all_ranks[@]}; do
   sed "${r}q;d" $alexa_1m | while read rank url; do
-   curl -d "{ \"rank\" : $rank, \"url\" : \"$url\" }" -H "Content-Type: application/json" $server_collection_url &> /dev/null
+   curl -d "{ \"rank\" : $rank, \"url\" : \"http://www.$url\" }" -H "Content-Type: application/json" $server_collection_url &> /dev/null
  done
 done
