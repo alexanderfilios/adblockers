5eb84f7f44744a21564ce1367c01e8405fbadc26
Alexandros Filios <alexanderfilios@hotmail.com>
2016-04-17 19:56:14 +0200

    Mobile user agent set by general.useragent.override config parameter instead of using an add-on. Hence the add-on no longer needs to be installed.

--- setup/install_addons.sh
+++ setup/install_addons.sh
@@ -12,7 +12,6 @@ declare -A xpis=(
   ["adblockplus"]="$setup_dir/addons/{d10d0bf8-f5b5-c8b4-a8b2-2b9879e08c5d}.xpi"
   ["ghostery"]="$setup_dir/addons/ghostery-6.1.0-sm+fx.xpi"
   ["lightbeam"]="$lightbeam_dir/*.xpi"
-  ["user_agent_switcher"]="$setup_dir/addons/user_agent_switcher-0.7.3-fx+sm.xpi"
 )
 
 declare -A profiles=(
@@ -22,12 +21,12 @@ declare -A profiles=(
   ["Adblockplus_MaxProtection"]="adblockplus lightbeam"
   ["NoAdblocker"]="lightbeam"
   ["NoAdblocker_DNT"]="lightbeam"
-  ["Ghostery_Default_MUA"]="ghostery lightbeam user_agent_switcher"
-  ["Ghostery_MaxProtection_MUA"]="ghostery lightbeam user_agent_switcher"
-  ["Adblockplus_Default_MUA"]="adblockplus lightbeam user_agent_switcher"
-  ["Adblockplus_MaxProtection_MUA"]="adblockplus lightbeam user_agent_switcher"
-  ["NoAdblocker_MUA"]="lightbeam user_agent_switcher"
-  ["NoAdblocker_DNT_MUA"]="lightbeam user_agent_switcher"
+  ["Ghostery_Default_MUA"]="ghostery lightbeam"
+  ["Ghostery_MaxProtection_MUA"]="ghostery lightbeam"
+  ["Adblockplus_Default_MUA"]="adblockplus lightbeam"
+  ["Adblockplus_MaxProtection_MUA"]="adblockplus lightbeam"
+  ["NoAdblocker_MUA"]="lightbeam"
+  ["NoAdblocker_DNT_MUA"]="lightbeam"
 )
 
 killall firefox
--- setup/setup_account.sh
+++ setup/setup_account.sh
@@ -30,6 +30,8 @@ else
   profiles=("$1")
 fi
 
+USER_AGENT="ozilla/5.0 (iPhone; CPU iPhone OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A5376e Safari/8536.25"
+
 set_config_param() {
   profile="$1"
   param_name="$2"
@@ -70,5 +72,11 @@ for profile in ${profiles[@]}; do
   set_config_param $profile "xpinstall.signatures.required" "false"
   # Allow popups from add-ons (lightbeam opens popups to crawl them)
   set_config_param $profile "dom.disable_open_during_load" "false"
+
+  # Set mobile user agent
+  if [[ $profile == *"MUA" ]]; then
+    set_config_param $profile "general.useragent.override" "\"$USER_AGENT\""
+  fi
+
   echo "Done!"
 done
