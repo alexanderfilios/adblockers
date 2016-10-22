1700b55bb58c971d2e89ebb8b0ce86219ab5ba61
Alexandros Filios <alexandrosfilios@Alexandross-MacBook-Air.local>
2016-10-22 17:12:33 +0200

    Cleaned up projects

--- statistics/server.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by alexandros on 3/27/16.
- */
-
-const connect = require('connect');
-const serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname + '/public')).listen(8080, function() {
-  console.log('Server running on 8080...');
-});
